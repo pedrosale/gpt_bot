03b34ed4f851da19e3c0d18b5ac6b6b6c091c9ff
pedrosale <53319245+pedrosale@users.noreply.github.com>
2024-03-20 08:59:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def main():
 
     if send_button and query:
         docs = knowledgeBase.similarity_search(query)
-        llm = OpenAI(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+        llm = OpenAI(openai_api_key=os.environ.get("OPENAI_API_KEY"), temperature=0.01, max_length=1000, top_p=1)
         chain = load_qa_chain(llm, chain_type="stuff")
 
         with get_openai_callback() as cost:
